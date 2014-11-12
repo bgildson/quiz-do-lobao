a90009c9feb553a5052739fb9ca5074ab3655882
gildson <gildson13@hotmail.com>
2014-11-12 11:31:31 -0300

    Login ok

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import Required, Length, EqualTo, Email
 
 class UsuarioRegistroForm(Form):
 	usuario = TextField('usuario', validators=[Required('O campo "Usuário" é obrigatório.'), Length(max=20, message='O nome do usuário deve conter no máximo 20 caracteres')])
-	email = TextField('email', validators=[Required('O campo "Senha" é obrigatório.'), Email('E-mail informado é inválido')])
+	email = TextField('email', validators=[Required('O campo "E-mail" é obrigatório.'), Email('E-mail informado é inválido')])
 	senha = PasswordField('senha', validators=[Required('O campo "Senha" é obrigatório.')])
 	confirma_senha = PasswordField('confirma_senha', validators=[Required('O campo "Confirmação de Senha" é obrigatório.'), EqualTo('senha', 'Preencha os campos de senha corretamente.')])
 
 class UsuarioLoginForm(Form):
-	usuario_ = TextField('usuario_', validators=[Required('O campo "Usuário" é obrigatório.')])
-	senha_ = PasswordField('senha_', validators=[Required('O campo "Senha" é obrigatório.')])
+	usuario = TextField('usuario', validators=[Required('O campo "Usuário" é obrigatório.')])
+	senha = PasswordField('senha', validators=[Required('O campo "Senha" é obrigatório.')])
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import render_template, request, redirect, url_for, flash
 from flask.ext.login import LoginManager, current_user, login_user, logout_user, login_required
 from app import app, db, forms
@@ -6,49 +7,60 @@ import hashlib
 
 lm = LoginManager()
 lm.init_app(app)
-lm.login_view = 'registro_login'
+lm.login_view = 'login'
+
+@lm.user_loader
+def load_user(id):
+	return db.session.query(Usuario).filter_by(_id=id).first()
 
 @app.route('/')
 @login_required
 def home():
-	return 'ok'
+	return render_template('index.html', title='Home')
 
-@app.route('/acesso', methods=['GET', 'POST'])
-def registro_login():
-	# cria os formularios presentes na pagina
-	form_login = forms.UsuarioLoginForm()
-	form_registro = forms.UsuarioRegistroForm()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = forms.UsuarioLoginForm()
 
-	# verifica se houve uma solicitacao de login
-	if form_login.validate_on_submit():
-		usuario = db.session.query(Usuario).filter_by(usuario=form_login.usuario_.data.lower(), senha=hashlib.md5(form_login.senha_.data.encode('utf-8')).hexdigest()).first()
+	if form.validate_on_submit():
+		usuario = db.session.query(Usuario).filter_by(usuario=form.usuario.data.lower(), senha=hashlib.md5(form.senha.data.encode('utf-8')).hexdigest()).first()
 		if usuario:
 			login_user(usuario)
-
-	# verifica se houve uma solicitacao de registro
-	if form_registro.validate_on_submit():
-		usuario = db.session.query(Usuario).filter_by(usuario=form_registro.usuario.data.lower()).first()
-		email = db.session.query(Usuario).filter_by(email=form_registro.email.data.lower()).first()
-
-		# verifica se ja nao existe algum usuario cadastrado com o mesmo nome ou email informados
-		if usuario:
-			flash('Usuário informado já existe.')
-		elif email:
-			flash('E-mail informado já esta sendo utilizado.')
 		else:
-			usuario = Usuario(form_registro.usuario.data, form_registro.email.data, form_registro.senha.data)
+			flash('Usuário ou Senha inválidos.', 'login')
+
+	if not current_user.is_anonymous() and current_user.is_authenticated():
+		return redirect( url_for('home') )
+
+	return render_template('usuario/login.html', form=form)
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+	form = forms.UsuarioRegistroForm()
+
+	if form.validate_on_submit():
+		usuario = db.session.query(Usuario).filter_by(usuario=form.usuario.data.lower()).first()
+		email = db.session.query(Usuario).filter_by(email=form.email.data.lower()).first()
+
+		if usuario:
+			flash('Usuário informado já existe.', 'registro')
+
+		if email:
+			flash('E-mail informado já esta sendo utilizado.', 'registro')
+
+		if not (usuario or email):
+			usuario = Usuario(form.usuario.data, form.email.data, form.senha.data)
 			db.session.add(usuario)
 			db.session.commit()
 			if usuario:
 				login_user(usuario)
 
-	# verifica se ja existe algum usuario autenticado
-	if current_user.is_authenticated() and not current_user.is_anonymous():
+	if not current_user.is_anonymous() and current_user.is_authenticated():
 		return redirect( url_for('home') )
 
-	return render_template('acesso.html', form_registro=form_registro, form_login=form_login)
+	return render_template('usuario/registro.html', form=form)
 
 @app.route('/logout')
 def logout():
 	logout_user()
-	return redirect( url_for('registro_login') )
+	return redirect( url_for('login') )
