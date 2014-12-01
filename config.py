dd5093475276a53a57a2da4f123d9ba99e97524f
Gildson <gildson13@hotmail.com>
2014-12-01 02:38:33 -0300

    Adicionando migration...

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+
+# db generico
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
 
 # forms
 CSRF_ENABLED = True
-SECRET_KEY = 'quiz-do-lobao-shadow-warriors'
--- manage.py
+++ manage.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-from quiz import app
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+from quiz import app, db
+
+
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+#manager.add_command('run', app.run())
+manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
